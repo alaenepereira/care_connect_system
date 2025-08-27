ac43a72338ffb675df81fab70c0926dde1a30178
alaenepereira <alaenepereira06@gmail.com>
2025-08-27 20:00:33 -0300

    feat: initialize back-end with Prisma, Express, and error handling
    - Added package.json for project dependencies and scripts
    - Created initial Prisma migration for User model with unique constraints
    - Set up Prisma schema for User model
    - Implemented custom AppError class for error handling
    - Established basic Express server with CORS and JSON parsing
    - Configured routes and error handling middleware

---
+++ back-end/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes/index.js'
+import AppError from './errors/AppError.js';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(routes)
+
+app.use((err, _request, response, next) => {
+  if(err instanceof AppError){
+    return response.status(err.statusCode).json({ message: err.message})
+  }
+
+  if (!err.statusCode) {
+    return response.status(500).json({
+      status: 'error',
+      message: `Erro interno do servidor - ${err.message}`,
+    });
+  }
+  return next();
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
